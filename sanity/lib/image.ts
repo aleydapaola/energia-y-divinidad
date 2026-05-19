35f03453d6f2bec0a577bf7d453444f30ae55349
Xavier Monfort <xmonfau@gmail.com>
2026-05-19 23:40:58 +0200

    fix(sanity): construir urls de imagen con dataset

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,9 +1,10 @@
 import createImageUrlBuilder from '@sanity/image-url'
 import type { Image } from 'sanity'
 
-import { client } from './client'
-
-const imageBuilder = createImageUrlBuilder(client)
+const imageBuilder = createImageUrlBuilder({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '81jpalr2',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+})
 
 export const urlForImage = (source: Image) => {
   return imageBuilder.image(source).auto('format').fit('max')
